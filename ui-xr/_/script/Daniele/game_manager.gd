6cc7bcc8b2a3110e7fbebdbb283138ddb36abd23
Daniele <s_13980_devxr@pedagogique.lan>
2026-06-04 08:41:15 +0200

    [Daniele] chronometer done

--- ui-xr/_/script/Daniele/game_manager.gd
+++ ui-xr/_/script/Daniele/game_manager.gd
@@ -17,12 +17,14 @@ var actual_state: game_state = game_state.NONE
 @export var cooldown_respawn_enemy: float = 5
 @export var player_bot: Node
 @export var player_collider: CollisionShape3D
+var chrono : float
 
 #var enemies_node: Array[Enemy]
 #var enemy_pool_system: PoolSystem
 
 func _ready() -> void:
 	actual_state = game_state.StartCoding
+	chrono = chronometer_in_second
 	if !player_bot:
 		return
 	if player_collider:
@@ -41,7 +43,11 @@ func _process(delta: float) -> void:
 		game_state.StartCoding:
 			start_game()
 		game_state.CodeRunning:
-			start_battle_bot()
+			chronometer_in_second -= delta
+			if player_bot_life < 0:
+				actual_state = game_state.GameOver
+			if chronometer_in_second < 0:
+				actual_state = game_state.Victory
 		game_state.GameOver:
 			game_over()
 		game_state.Victory:
@@ -53,10 +59,7 @@ func start_game():
 	pass
 
 func start_battle_bot():
-	if player_bot_life < 0:
-		actual_state = game_state.GameOver
-	if chronometer_in_second < 0:
-		actual_state = game_state.Victory
+	pass
 	#for enemy in enemies_node:
 		#pass
 		#if enemy.isDead()
@@ -73,10 +76,12 @@ func victory():
 	pass
 
 func on_run_pressed():
+	#don't know if game_manager should check validation of code modded or if modding script do it
 	actual_state = game_state.CodeRunning
 
 func on_retry_pressed():
 	actual_state = game_state.StartCoding
+	chronometer_in_second = chrono
 	player_bot_life = 3
 
 func get_enemies(enemies_node: Array[Node]):
