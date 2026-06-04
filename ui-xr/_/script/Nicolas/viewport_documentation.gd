774787f262bdc13f8e4b102e2eca89ae7c4e4058
Carlier Nicolas <n.carlier6000@gmail.com>
2026-06-04 10:50:09 +0200

    [ncar] Documentation integration done

--- ui-xr/_/script/Nicolas/viewport_documentation.gd
+++ ui-xr/_/script/Nicolas/viewport_documentation.gd
@@ -3,7 +3,12 @@ extends Node3D
 @onready var quad: MeshInstance3D = $size/Quad
 @onready var sub_viewport: SubViewport = $size/SubViewport
 
+signal send_text_to_control(text : String)
+
 
 func _ready() -> void:
 	var material :StandardMaterial3D = quad.get_active_material(0)
 	material.albedo_texture = sub_viewport.get_texture()
+	
+func emit_text_to_control(text : String) -> void:
+	send_text_to_control.emit (text)
